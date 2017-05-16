39ec05eb929342e5bd9c3f040d667bb7c6f7a896
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-05-16 13:59:12 +0200

    Helpers for browser-core

--- vars/xunit.groovy
+++ vars/xunit.groovy
@@ -1,11 +1,7 @@
 #!/usr/bin/env groovy
 
-def parse(Map vars) {
-    if (vars == null || !vars.reportPath) {
-        error 'Parameter <reportPath> is required for parseJunitReport'
-    }
-
-    def result = sh(returnStdout: true, script: "xmllint --xpath '//testsuite/@*' ${vars.reportPath}").trim()
+def parse(reportPath) {
+    def result = sh(returnStdout: true, script: "xmllint --xpath '//testsuite/@*' ${reportPath}").trim()
     def lstResult = result =~ /([^=]+)="([^"]+)"[\s]*/
 
     def summary = [:]
@@ -15,6 +11,15 @@ def parse(Map vars) {
       }
     }
     return summary
+}
+
+// source: https://issues.jenkins-ci.org/browse/JENKINS-27395?focusedCommentId=256459&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-256459
+def setJUnitPackageName(packageName, inFile, outFile) {
+    // Prepend the testName as the package name so that we get sorted output in the
+    // Jenkins test results page. The packageName is extracted from the classname
+    // attribute of the testcase tag.
+    // WARNING: the package attribute of the testcase tag is igonred by Jenkins
+    sh "sed \"s/\\(classname=['\\\"]\\)/\\1${packageName}./g\" ${inFile} > ${outFile}"
 }
 
 return this
