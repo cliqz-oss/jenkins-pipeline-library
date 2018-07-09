5ea303e4346c5f7a05ed987f09067c4da88f68ce
joseph-cliqz <joseph@cliqz.com>
2018-07-09 15:18:30 +0200

    Update jira helpers

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,35 +1,52 @@
 #!/usr/bin/env groovy
 
+def lastSuccessfulBuild(passedBuilds, build) {
+    if ((build != null) && (build.result != 'SUCCESS')) {
+        echo "Finding last succesful build"
+        passedBuilds.add(build)
+        echo "Found a build"
+        lastSuccessfulBuild(passedBuilds, build.getPreviousBuild())
+    }
+}
+
 @NonCPS
-def getChangeString() {
-    MAX_MSG_LEN = 100
+def getChangeString(passedBuilds) {
     def changeString = ""
-    echo "Gathering SCM changes"
-    def changeLogSets = currentBuild.changeSets
-    for (int i = 0; i < changeLogSets.size(); i++) {
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-            def entry = entries[j]
-            truncated_msg = entry.msg.take(MAX_MSG_LEN)
-            changeString += " - ${truncated_msg} [${entry.author}]\n"
+    echo "Gathering Changes Since Last Successful Build"
+    for (int x = 0; x < passedBuilds.size(); x++) {
+        def currentBuild = passedBuilds[x];
+        def buildNumber = currentBuild.number
+        echo "Changes for Build ${buildNumber}"
+        def changeLogSets = currentBuild.rawBuild.changeSets
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                changeString += "* ${entry.msg} by ${entry.author} \n"
+            }
         }
     }
     if (!changeString) {
         changeString = " - No new changes"
     }
-    return changeString
+    echo changeString
+    return changeString;
 }
 
 @NonCPS
-def getIssueList() {
+def getIssueList(){
     def list = []
-    def changes = getChangeString()
-    def re = /(AB|DB|EX|ICE|IB)-([0-9])*/
+    def changes = getChangeString(passedBuilds)
+    def re = /(AB|DB|EX|ICE|IB|AB2|IB2)-([0-9])*/
     def y = changes =~ re
-    while (y) {
+    while (y){
         list.add(y.group().toString())
     }
-    echo 'Detected JIRA tickets'
-    echo list
+    if (list.size() > 0) {
+        echo 'Detected JIRA tickets'
+        echo list.toString()
+    } else {
+       echo 'No JIRA tickets detected'
+    }
     return list
 }
